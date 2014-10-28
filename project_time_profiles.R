9567df2cea5a12c306def76222ad53ea263c6dc9
mike breecher <michaelbreecher@gmail.com>
2014-10-28 11:39:12 -0700

    initial commit

---
+++ project_time_profiles.R
@@ -0,0 +1,22 @@
+library(RMySQL)
+setwd("C:/R/workspace")
+source("get_query.r")
+setwd("C:/R/workspace/workload_profile")
+
+#grab project and collapsed time data from mysql database
+con <- dbConnect(dbDriver("MySQL"), user = "root", password = "", dbname = "revenue_analysis")
+
+sql <- paste("select subcloud.service_id, subcloud.opportunity_id, timelog.is_psm, subcloud.account_name, subcloud.cik, subcloud.registrant_type, 
+             subcloud.solution, subcloud.SrPSM, subcloud.PSM, subcloud.CSM, subcloud.Sr_CSM, subcloud.service_name, subcloud.cs_ps, 
+             subcloud.service_type, subcloud.form, subcloud.quarter_end, subcloud.filing_date, subcloud.filing_deadline, subcloud.filing_deadline_recalc,
+             subcloud.service_status, subcloud.customer_status, subcloud.year_end, subcloud.reporting_period, subcloud.service_period, subcloud.list_price, 
+             subcloud.sales_price, timelog.Billable, subcloud.filing_week_num, logged_week_num, relative_week_num, sum(timelog.hours) 
+             from subcloud left join timelog 
+             on subcloud.service_id collate latin1_bin = timelog.service_id collate latin1_bin
+             where subcloud.service_id like 'a0%' and service_status = 'Completed' and is_psm = 1
+             group by subcloud.service_id, subcloud.account_name, timelog.is_psm, relative_week_num", sep = "")                
+
+query <- dbGetQuery(con, sql)
+dbDisconnect(con)
+
+names(query)[names(query) == "sum(timelog.hours)"] <- "time"
